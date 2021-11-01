046f31c4621a7aef76309dcde716716bbdc0704f
Richard Maxwell <richard@rsmaxwell.co.uk>
2021-11-01 20:01:37 +0000

    add denug to prepare script

--- build.sh
+++ build.sh
@@ -7,4 +7,14 @@ cd "./src/github.com/rsmaxwell/players-tt"
 pwd
 ls -al 
 
+ng add --skip-confirmation --defaults @angular/material
+npm install @angular/cdk
+npm install @angular/flex-layout
+npm install @angular/animations
+npm install bootstrap
+npm install hammerjs
+npm install hammerjs
+npm install ngx-mqtt
+npm install guid-typescript
+
 ng build
