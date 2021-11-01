231ceb0a92c8b673e90b0212ee0c90b11b73a6e9
Richard Maxwell <richard@rsmaxwell.co.uk>
2021-11-01 21:46:26 +0000

    add debug to prepare script

--- deploy.sh
+++ deploy.sh
@@ -22,8 +22,8 @@ rm -rf deploy
 mkdir -p deploy
 
 cd dist/players-tt
-zip ../deploy/${FILENAME} *
+zip ../../deploy/${FILENAME} *
 
-cd ../deploy
+cd ../../deploy
 mvn --batch-mode deploy:deploy-file -DgroupId=${GROUPID} -DartifactId=${ARTIFACTID} -Dversion=${VERSION} -Dpackaging=${PACKAGING} -Dfile=${FILENAME} -DrepositoryId=${REPOSITORYID} -Durl=${URL} -DrepositoryId=${REPOSITORYID}
 
